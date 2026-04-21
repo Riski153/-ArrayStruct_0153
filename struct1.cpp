92653f882d3e4c8571dd486546f086ccef12df3e
Riski153 <m.riski.f21@mail.umy.ac.id>
2026-04-21 09:59:56 +0700

    menambahkan int main

--- struct1.cpp
+++ struct1.cpp
@@ -8,3 +8,23 @@ struct Mahasiswa {
     string alamat;
     int umur;
 };
+
+int main() {
+    Mahasiswa mhs;
+
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.NIM;
+
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+
+    cout << "Alamat Mahasiswa (Desa) : ";
+    cin >> mhs.alamat;
+
+    cout << "Alamat Mahasiswa (Kota) : ";
+    cin >> mhs.alamat;
+
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+}
